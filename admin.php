c6edde92b6b640a6777ec4b6479e086ff31a2c27
Carlos Stenzel <betinhostenzel@hotmail.com>
2015-01-27 15:07:09 -0200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include("conecta.php");
+Verifica();
+
+/*  ##           **   ##
+   ##           **     ##
+  ##           **       ##
+ ##   ---  ----  ---  ----  -  -   -   --   --  ---  ---- ---
+##    |    |  |  |  | |---  |--|  /_\  | \ / |  |__| |--- |__|
+ ##   ---  ----  ---  ----  -  - -   - -  -  -  -    ---- - \
+  ##       **           ##  {.com}
+   ##     **           ##
+    ##   **          ##  */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title></title>
+</head>
+<body>
+  <h1>Logado</h1>
+</body>
+</html>
